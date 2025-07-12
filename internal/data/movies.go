e2310ae7ee2467a755f4f6f561bf1753e0f91b1b
Jayant Gupta <jayant.gupta.dln@gmail.com>
2025-07-12 19:12:19 +0530

    Add custom json converter for runtime

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -6,7 +6,7 @@ type Movie struct {
 	ID        int64     `json:"id"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitzero"`
-	RunTime   int32     `json:"runtime,omitzero,string"` // string directive would convert value to string
+	RunTime   Runtime     `json:"runtime,omitzero"` // string directive would convert value to string
 	Genres    []string  `json:"genres"`
 	Version   int32     `json:"version"`
 	CreatedAt time.Time `json:"-"`
