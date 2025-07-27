dbdb487b54bab34d001ad65af3bf03291bee5980
Jayant Gupta <jayant.gupta.dln@gmail.com>
2025-07-27 12:28:59 +0530

    Add movies migrations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -31,13 +31,14 @@ type application struct {
 	logger *slog.Logger
 }
 
+// "postgres://postgres:mysecretpassword@localhost:5432/greenlight?sslmode=disable"
 func main() {
 
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://postgres:mysecretpassword@localhost:5432/greenlight?sslmode=disable", "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 
 	// hardcoding for now
 	cfg.db.maxIdleConns = 25
